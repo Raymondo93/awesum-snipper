d024b7014691817afd3e5887bc94b1960c3127af
Raymond de Bruine <raymond.debruine@gmail.com>
2022-07-03 05:39:25 +0200

    tile atom

--- src/components/atoms/Tile.vue
+++ src/components/atoms/Tile.vue
@@ -8,9 +8,11 @@ import StockImage from "@/assets/images/pexels-pixabay-162140.jpg";
       <img class="tile-image" :src="StockImage" alt="tile-image" width="" height="" />
 
     </div>
-    <h3 class="tile-title">title</h3>
-    <p class="tile-content">Prow scuttle parrel provost Sail ho shrouds spirits boom mizzenmast yardarm. Pinnace holystone mizzenmast quarter crow's nest nipperkin grog yardarm hempen halter furl. Swab barque interloper chantey doubloon starboard grog black jack gangway rutters.
-      Deadlights jack lad schooner scallywag dance the hempen jig carouser broadside cable strike colors. Bring a spring upon her cable holystone blow the man down spanker Shiver me timbers to go on account lookout wherry doubloon chase. Belay yo-ho-ho keelhaul squiffy black spot yardarm spyglass sheet transom heave to.
-    </p>
+    <div class="tile-content-box">
+      <h3 class="tile-title">title</h3>
+      <p class="tile-content">Prow scuttle parrel provost Sail ho shrouds spirits boom mizzenmast yardarm. Pinnace holystone mizzenmast quarter crow's nest nipperkin grog yardarm hempen halter furl. Swab barque interloper chantey doubloon starboard grog black jack gangway rutters.
+        Deadlights jack lad schooner scallywag dance the hempen jig carouser broadside cable strike colors. Bring a spring upon her cable holystone blow the man down spanker Shiver me timbers to go on account lookout wherry doubloon chase. Belay yo-ho-ho keelhaul squiffy black spot yardarm spyglass sheet transom heave to.
+      </p>
+    </div>
   </div>
 </template>
