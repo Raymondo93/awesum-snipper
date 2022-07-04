b42122ffc09747861d6d9a4969a6f03725f344fe
Raymond de Bruine <raymond.debruine@gmail.com>
2022-07-04 03:14:00 +0200

    added check for special selectors

--- src/stores/snippet.js
+++ src/stores/snippet.js
@@ -1,8 +1,8 @@
 import {defineStore} from 'pinia';
 import formatStringToHtml from 'format-string-to-html';
 import css from '@/assets/style/style.css?raw';
-import {parseToJson} from "../assets/js/cssToJsonParser";
-import {getClassList} from "../assets/js/classListCrawler";
+import {parseToJson} from "@/assets/js/cssToJsonParser";
+import {getClassList} from "@/assets/js/classListCrawler";
 
 export const useSnippetStore = defineStore({
   id: 'snippet',
@@ -14,14 +14,14 @@ export const useSnippetStore = defineStore({
   }),
   actions: {
     getSnippet(e) {
-      if(this.component !== '') {
+      if (this.component !== '') {
         this.resetStore(this.component);
       }
       this.component = e.target.value;
       let code = document.getElementById(this.component);
       code.style.display = 'block';
       this.htmlSnippet = new formatStringToHtml(code.getInnerHTML()).format();
-      this.getCssSnippet(document.getElementById(this.component));
+      this.getCssSnippet(code);
     },
     getCssSnippet(element) {
       let usedClassList = getClassList(element);
@@ -29,17 +29,28 @@ export const useSnippetStore = defineStore({
       let classes = [];
       usedClassList.forEach((cssClass) => {
         for (let i = 0; i < cssJson.length; ++i) {
-          if ('.' + cssClass === cssJson[i].selectors.trim()) {
+          let selector = cssJson[i].selectors.trim();
+          if (selector.includes(':')) {
+            let s = selector.split(':');
+            selector = s[0];
+          }
+          if ('.' + cssClass === selector) {
             classes.push(cssJson[i]);
           }
         }
-      })
+      });
       this.cssSnippet = classes;
+    },
+    updateSnippet() {
+      let component = document.getElementById(this.component);
+      this.htmlSnippet = new formatStringToHtml(component.getInnerHTML()).format();
+      this.getCssSnippet(component);
+      console.log(this.htmlSnippet);
     },
     resetStore(component) {
       document.getElementById(component).style.display = 'none';
       this.$reset();
-    }
+    },
   }
 });
 
