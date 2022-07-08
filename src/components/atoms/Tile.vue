b1f6094c23d7616b1b92ab4c82bc7d8a326d40f9
Raymond de Bruine <raymond.debruine@gmail.com>
2022-07-08 12:10:30 +0200

    added picturebox to tiles, options into tiles and some styling

--- src/components/atoms/Tile.vue
+++ src/components/atoms/Tile.vue
@@ -1,6 +1,7 @@
 <script setup>
 import StockImage from "@/assets/images/pexels-pixabay-162140.jpg";
-import {useSnippetStore} from "../../stores/snippet";
+import FallbackImage from "@/assets/images/jake-weirick-1Ad511F-ia0-unsplash.jpg";
+import {useSnippetStore} from "@/stores/snippet";
 import {computed, onUpdated} from "vue";
 
 const snippetStore = useSnippetStore();
@@ -37,15 +38,18 @@ onUpdated(() => {
 <template>
   <div class="tile" :class="setQuantity">
     <div class="tile-box">
-      <div class="tile-image-box" v-if="props.tile.hasImage">
-        <img class="tile-image" :src="StockImage" alt="tile-image" width="" height="" />
+      <div class="tile-image-box" v-if="this.props.tile.hasImage">
+        <picture class="tile-image">
+          <source media="(min-width:375px)" :srcset="StockImage" />
+          <img :src="FallbackImage" alt="tile-image" width="" height="" />
+        </picture>
       </div>
       <div class="tile-content-box">
-        <h3 class="tile-title">{{props.tile.title}}</h3>
-        <ul class="tile-list" v-if="props.tile.hasList">
-          <li class="tile-list-item" v-for="item in props.tile.tileItems">{{item}}</li>
+        <h3 class="tile-title">{{this.props.tile.title}}</h3>
+        <ul class="tile-list" v-if="this.props.tile.hasList">
+          <li class="tile-list-item" v-for="item in this.props.tile.tileItems">{{item}}</li>
         </ul>
-        <p v-else class="tile-content">{{props.tile.tileContent}}</p>
+        <p v-else class="tile-content">{{this.props.tile.tileContent}}</p>
       </div>
     </div>
   </div>
