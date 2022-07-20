78d989056cfb159f5d11d2583dd6d5ec4e3a28df
Raymond de Bruine <raymond.debruine@gmail.com>
2022-07-20 04:11:57 +0200

    banner options

--- src/components/options/BannerOptions.vue
+++ src/components/options/BannerOptions.vue
@@ -8,5 +8,17 @@ const bannerStore = useBannerStore();
   <div class="banner-options">
     <label :for="'banner-height'">Height banner</label>
     <input name="banner-height" type="number" @change="bannerStore.updateBannerHeight" />
+    <label :for="'banner-image-width'">Banner image full width?</label>
+    <input type="checkbox" name="banImgWidth" @change="bannerStore.toggleBannerImageWidth" checked />
+
+    <div v-if="!bannerStore.isFullWidth">
+      <label :for="'banner-image-position'">Image position</label>
+      <select class="selectbox" @change="bannerStore.toggleImagePosition">
+        <option value="is-right">right</option>
+        <option value="is-left">left</option>
+      </select>
+
+      <input type="checkbox" name="banHasButton" @change="bannerStore.toggleButton" checked />
+    </div>
   </div>
 </template>
