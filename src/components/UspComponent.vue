c188561658d43e7b8d7a55881f64c4c6ae49a43b
Raymond de Bruine <raymond.debruine@gmail.com>
2022-07-21 02:30:49 +0200

    usp component

---
+++ src/components/UspComponent.vue
@@ -0,0 +1,24 @@
+<script setup>
+import ButtonComponent from "./ButtonComponent.vue";
+import {useUspStore} from "@/stores/usp.js";
+
+const uspStore = useUspStore();
+</script>
+
+<template>
+  <div id="component-usp">
+    <div class="usp">
+      <ul class="usp-list">
+        <li class="usp-box" v-for="u in uspStore.usp">
+          <p class="usp-box-icon">
+            {{ u.icon }}
+          </p>
+          <div class="usp-box-content">
+            <h3 class="usp-box-title">{{ u.title }}</h3>
+            <p class="usp-box-description">{{ u.description }}</p>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
